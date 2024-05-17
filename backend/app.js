fa2bb4ed2e40ec221a3bdaa67bca2237842674a1
Code-Gale <codewhiz001@gmail.com>
2024-05-17 21:00:43 +0100

    updated app

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,12 @@ const connectDB = require('./config/db')
 
 const app = express()
 
+//middlewares
 connectDB()
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
 
+//listening for requests
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log('Listening for requests on', PORT)
